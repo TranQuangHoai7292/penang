3febc6987a7852360ff774f6967e251309e43ff1
tranquanghoai <hoaitranquang7292@gmail.com>
2019-10-11 17:13:06 +0700

    push code

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -3,7 +3,7 @@
         Xin lỗi vì sự bất tiện này!!!. Chương trình vẫn chưa bắt đầu, mời bạn quay lại trong:<br><br>
         <span id="timer" style="color:red;font-size: 20px;font-weight: 900"></span>
     </h5>
-    <form action="{{route('check.anwer')}}" method="POST" id="question" >
+    <form action="{{route('check.anwer')}}" method="POST" id="question" class="block none">
         @csrf
         <?php $stt =1 ; ?>
         @foreach ($q as $foryou)
@@ -49,54 +49,54 @@
 
         ?>
     </form>
-{{--    <script>--}}
-{{--        $(document).ready(function(){--}}
-{{--            var starline = new Date ("October 15, 2019 11:45:00");--}}
-{{--            var now = new Date();--}}
-{{--            var timeup = starline.setSeconds(starline.getSeconds());--}}
-{{--            var counter = setInterval(timer, 1000);--}}
-{{--            function timer() {--}}
-{{--                now = new Date();--}}
-{{--                count = Math.round((timeup - now) / 1000);--}}
-{{--                if (now >= timeup) {--}}
+    <script>
+        $(document).ready(function(){
+            var starline = new Date ("October 15, 2019 11:45:00");
+            var now = new Date();
+            var timeup = starline.setSeconds(starline.getSeconds());
+            var counter = setInterval(timer, 1000);
+            function timer() {
+                now = new Date();
+                count = Math.round((timeup - now) / 1000);
+                if (now >= timeup) {
 
-{{--                    var change = 'Hãy trả lời đúng câu hỏi kiểm tra văn hóa dành cho bạn. Cố gắng suy nghĩ kỹ và trả lời đúng nhé. Chúc bạn may mắn và đây là 10 câu hỏi của bạn:<br><br>\n' +--}}
-{{--                        '        Thời gian để hoàn thành là trước 12h. Hãy chú ý đồng hồ đếm ngược nhé.<br><br>\n' +--}}
-{{--                        '        <span id="timer" style="color:red;font-size: 20px;font-weight: 900"></span>';--}}
-{{--                    $('.time-star').html(change);--}}
-{{--                    $('.block').removeClass('none');--}}
-{{--                    var starline1 = new Date ("October 15, 2019 12:00:00");--}}
-{{--                    var now1 = new Date();--}}
-{{--                    var timeup1 = starline1.setSeconds(starline1.getSeconds());--}}
-{{--                    var counter1 = setInterval(timer, 1000);--}}
-{{--                    function timer() {--}}
-{{--                        now1 = new Date();--}}
-{{--                        count1 = Math.round((timeup1 - now1) / 1000);--}}
-{{--                        if (now1 > timeup1) {--}}
-{{--                            var url = '{{route('overtime')}}';--}}
-{{--                            var code = $.cookie('code');--}}
-{{--                            var token = $('meta[name="csrf-token"]').attr('content');--}}
-{{--                            $.post(url,{'code':code,'_token':token},function(data){--}}
-{{--                                if (data == 1){--}}
-{{--                                    window.location = '{{route('index')}}'--}}
-{{--                                }--}}
-{{--                            });--}}
-{{--                            clearInterval(counter1);--}}
-{{--                            return;--}}
-{{--                        }--}}
-{{--                        var hours = Math.floor((count1 / 60 / 60));--}}
-{{--                        var seconds = Math.floor((count1 % 60));--}}
-{{--                        var minutes = Math.floor((count1 / 60) % 60);--}}
-{{--                        document.getElementById("timer").innerHTML = hours + " GIỜ " + minutes + " PHÚT " + seconds + " GIÂY";--}}
-{{--                    }--}}
-{{--                    clearInterval(counter);--}}
-{{--                    return;--}}
-{{--                }--}}
-{{--                var hours = Math.floor((count / 60 / 60));--}}
-{{--                var seconds = Math.floor((count % 60));--}}
-{{--                var minutes = Math.floor((count / 60) % 60);--}}
-{{--                document.getElementById("timer").innerHTML = hours + " GIỜ " + minutes + " PHÚT " + seconds + " GIÂY";--}}
-{{--            }--}}
-{{--        });--}}
-{{--    </script>--}}
+                    var change = 'Hãy trả lời đúng câu hỏi kiểm tra văn hóa dành cho bạn. Cố gắng suy nghĩ kỹ và trả lời đúng nhé. Chúc bạn may mắn và đây là 10 câu hỏi của bạn:<br><br>\n' +
+                        '        Thời gian để hoàn thành là trước 12h. Hãy chú ý đồng hồ đếm ngược nhé.<br><br>\n' +
+                        '        <span id="timer" style="color:red;font-size: 20px;font-weight: 900"></span>';
+                    $('.time-star').html(change);
+                    $('.block').removeClass('none');
+                    var starline1 = new Date ("October 15, 2019 12:00:00");
+                    var now1 = new Date();
+                    var timeup1 = starline1.setSeconds(starline1.getSeconds());
+                    var counter1 = setInterval(timer, 1000);
+                    function timer() {
+                        now1 = new Date();
+                        count1 = Math.round((timeup1 - now1) / 1000);
+                        if (now1 > timeup1) {
+                            var url = '{{route('overtime')}}';
+                            var code = $.cookie('code');
+                            var token = $('meta[name="csrf-token"]').attr('content');
+                            $.post(url,{'code':code,'_token':token},function(data){
+                                if (data == 1){
+                                    window.location = '{{route('index')}}'
+                                }
+                            });
+                            clearInterval(counter1);
+                            return;
+                        }
+                        var hours = Math.floor((count1 / 60 / 60));
+                        var seconds = Math.floor((count1 % 60));
+                        var minutes = Math.floor((count1 / 60) % 60);
+                        document.getElementById("timer").innerHTML = hours + " GIỜ " + minutes + " PHÚT " + seconds + " GIÂY";
+                    }
+                    clearInterval(counter);
+                    return;
+                }
+                var hours = Math.floor((count / 60 / 60));
+                var seconds = Math.floor((count % 60));
+                var minutes = Math.floor((count / 60) % 60);
+                document.getElementById("timer").innerHTML = hours + " GIỜ " + minutes + " PHÚT " + seconds + " GIÂY";
+            }
+        });
+    </script>
 
